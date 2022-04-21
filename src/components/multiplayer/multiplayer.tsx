d0a1008ea39cbfe67e31f36cf022e1ccaf9e3a87
CrossyChainsaw <kaangogcay@gmail.com>
2022-04-21 13:36:39 +0200

    You can input a word and it gets checked on existance

--- src/components/multiplayer/multiplayer.tsx
+++ src/components/multiplayer/multiplayer.tsx
@@ -1,12 +1,50 @@
-import { stat } from 'fs';
-import React, { useState } from 'react';
-import { text } from 'stream/consumers';
+import React, { useEffect, useState } from 'react';
+import { Word } from '../../interfaces/interfaces';
 import './multiplayer.css';
-import { Button } from "react-bootstrap";
+import './multiplayer.css';
 
-function Multiplayer() {
+function Multiplayer(props: any) {
+    const [word, setWord] = useState<string>("");
+    const [apiResult, setApiResult] = useState<Word>();
+    const [disabledTextBox, setDisabledTextBox] = useState<boolean>(false);
+    const [disabledButton, setDisabledButton] = useState<boolean>(false);
+    const [buttonClicks, setButtonClicks] = useState<number>(0);
+
+    useEffect(() => {
+        const api = async () => {
+            const apiUrl = "http://localhost:8080/word/test?word=" + word;
+            const data = await fetch(apiUrl);
+            const jsonData = await data.json();
+            setApiResult(jsonData);
+        };
+        api();
+    }, [buttonClicks]);
+
+    function OnChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setWord(e.currentTarget.value);
+    }
+
+    function OnCLick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        setButtonClicks(buttonClicks + 1);
+
+        console.log("word: " + word);
+        console.log("apiRes: " + apiResult?.word);
+
+        if (word.valueOf() == apiResult?.word.valueOf()) {
+            setDisabledTextBox(true);
+            setDisabledButton(true);
+        }
+        else {
+            console.log("the word '" + word + "' does not exist");
+        }
+    }
+
     return (
-        <h1>this is multiplayer</h1>
+        <div>
+            <h1>this is multiplayer</h1>
+            <input id='myTb' type='password' disabled={disabledTextBox} onChange={(e) => OnChange(e)} />
+            <button id='myBtn' disabled={disabledButton} onClick={(e) => OnCLick(e)}>Confirm</button>
+        </div>
     )
 }
 
--- src/components/singleplayer/singleplayer.tsx
+++ src/components/singleplayer/singleplayer.tsx
@@ -1,26 +1,48 @@
-import { stat } from 'fs';
-import React, { useState } from 'react';
-import { text } from 'stream/consumers';
+import React, { useEffect, useState } from 'react';
+import { Word } from '../../interfaces/interfaces';
 import './singleplayer.css';
-import { Button } from "react-bootstrap";
 
 function Singleplayer(props: any) {
-    const [word, setWord] = useState("");
-    const [enabled, setEnabled] = useState(false);
+    const [word, setWord] = useState<string>("");
+    const [apiResult, setApiResult] = useState<Word>();
+    const [disabledTextBox, setDisabledTextBox] = useState<boolean>(false);
+    const [disabledButton, setDisabledButton] = useState<boolean>(false);
+    const [buttonClicks, setButtonClicks] = useState<number>(0);
+
+    useEffect(() => {
+        const api = async () => {
+            const apiUrl = "http://localhost:8080/word/check?word=" + word;
+            const data = await fetch(apiUrl);
+            const jsonData = await data.json();
+            setApiResult(jsonData);
+        };
+        api();
+    }, [buttonClicks]);
+
+    useEffect(() => {
+        console.log(apiResult);
+        if (apiResult?.exists) {
+            setDisabledTextBox(true);
+            setDisabledButton(true);
+        }
+        else {
+            console.log("the word '" + word + "' does not exist");
+        }
+    }, [apiResult])
 
     function OnChange(e: React.ChangeEvent<HTMLInputElement>) {
         setWord(e.currentTarget.value);
     }
+
     function OnCLick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        setEnabled(true);
-        console.log(word);
+        setButtonClicks(buttonClicks + 1);
     }
 
     return (
         <div>
             <h1>this is singleplayer</h1>
-            <input id='myTb' type='password' disabled={enabled} onChange={(e) => OnChange(e)} />
-            <button id='myBtn' onClick={(e) => OnCLick(e)}>Confirm</button>
+            <input id='myTb' type='password' disabled={disabledTextBox} onChange={(e) => OnChange(e)} />
+            <button id='myBtn' disabled={disabledButton} onClick={(e) => OnCLick(e)}>Confirm</button>
         </div>
     )
 }
